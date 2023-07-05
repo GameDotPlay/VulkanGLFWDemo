b412b091c90d013769575903d3ebe1261d5d3292
Erich Smith <gamedotplay0@gmail.com>
2023-07-04 21:23:34 -0400

    Added members for initialization.

--- Application/src/private/Application.cpp
+++ Application/src/private/Application.cpp
@@ -1,29 +1,80 @@
 #include "../public/Application.h"
 #include "../../../Renderer/src/public/Renderer.h"
-#include "SDL2/SDL_vulkan.h"
 
-Application::Application(const std::string& appName, const std::string& engineName)
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include <stdexcept>
+
+Application::Application(const std::string& appName, const std::string& engineName, int width, int height)
 {
+	this->appName = appName;
+	this->engineName = engineName;
+	this->windowWidth = width;
+	this->windowHeight = height;
+
 	this->initWindow();
 	this->initRenderer();
 }
 
 void Application::run()
 {
-
+	this->mainLoop();
 }
 
 Application::~Application()
 {
-
+	delete this->renderer;
+	glfwTerminate();
 }
 
 void Application::initWindow()
 {
+	glfwInit();
 
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Don't create an OpenGL context.
+
+	this->window = glfwCreateWindow(this->windowWidth, this->windowHeight, this->appName.c_str(), nullptr, nullptr);
+	glfwSetWindowUserPointer(this->window, this);
+	glfwSetFramebufferSizeCallback(this->window, framebufferSizeCallback);
 }
 
 void Application::initRenderer()
 {
+	this->initVulkan();
+}
 
+void Application::initVulkan()
+{
+	std::vector<const char*> extensions = this->getRequiredExtensions();
+	this->renderer = new Renderer(this->appName, this->engineName, extensions);
+
+	if (this->renderer->getInstance() == VK_NULL_HANDLE)
+	{
+		throw std::runtime_error("Vulkan instance could not be created!");
+	}
+}
+
+void Application::mainLoop()
+{
+	while (!glfwWindowShouldClose(this->window))
+	{
+		glfwPollEvents();
+	}
+}
+
+std::vector<const char*> Application::getRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	return extensions;
+}
+
+void Application::framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	auto app = reinterpret_cast<Application*>(glfwGetWindowUserPointer(window));
+	app->windowResized = true;
 }
--- Application/src/public/Application.h
+++ Application/src/public/Application.h
@@ -1,24 +1,44 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Renderer;
+struct GLFWwindow;
 
 class Application
 {
 public:
 
-	Application(const std::string& appName, const std::string& engineName);
+	Application(const std::string& appName, const std::string& engineName, int width = 1280, int height = 720);
 	void run();
 	~Application();
 
 private:
 
+	struct VulkanInstanceInitInfo
+	{
+		const char* applicationName;
+		const char* engineName;
+		uint32_t extensionCount;
+		const char** requiredExtensions;
+	};
+
+	GLFWwindow* window = nullptr;
 	Renderer* renderer = nullptr;
 
+	bool windowResized = false;
+	int windowWidth = 1280;
+	int windowHeight = 720;
 	std::string appName;
 	std::string engineName;
 
 	void initWindow();
 	void initRenderer();
+	void initVulkan();
+	void mainLoop();
+
+	std::vector<const char*> getRequiredExtensions();
+
+	static void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 };
