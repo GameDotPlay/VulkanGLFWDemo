86bc6a598d5b98842d217cec76e7b2afc4266217
Erich Smith <gamedotplay0@gmail.com>
2023-07-10 21:53:50 -0400

    Removed includes for glm since they are not needed yet.

--- Renderer/src/Renderer.cpp
+++ Renderer/src/Renderer.cpp
@@ -1,18 +1,5 @@
 #include "Renderer.h"
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/hash.hpp>
-
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
-
-#define TINYOBJLOADER_IMPLEMENTATION
-#include <tiny_obj_loader.h>
-
 #include <stdexcept>
 #include <cstring>
 #include <vector>
